928a88abcded975a98cb884c79d0bc166b66f3a8
Najeeb Abdalla <najeeb040@gmail.com>
2024-05-20 11:24:13 +0300

    first

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,8 @@
+describe("home page", () => {
+	it("the h1 contains the correct text", () => {
+		cy.visit("http://localhost:3000");
+		cy.get("[data-test='hero-heading']").contains(
+			"Testing Next.js Applications with Cypress",
+		);
+	});
+});
